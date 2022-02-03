1a36053b3be60df22d3f18c8abdebe426f54562e
Obafemi Lawal <swissbobo@gmail.com>
2022-02-03 17:13:41 +0300

    will continue on my free time commit 1

---
+++ read.py
@@ -0,0 +1,37 @@
+import json
+from textwrap import indent
+
+
+if __name__ == "__main__":
+    with open('data/data_1.json') as f:
+        data = json.load(f)
+    
+    
+    def reader(data, result=None):
+        if not result:
+            result={}
+        if isinstance(data, dict):
+            dataArr = data.keys()
+        else:
+            dataArr = data
+        itemlength = len(dataArr)
+        while(itemlength > 0):
+            for i, v in enumerate(dataArr):
+                if isinstance(v, list): # i could add a generic iter here but not enough time
+                    result = []
+                    reader(v, result)
+                elif isinstance(data[v], dict): # specifically check for object so we can add it to result
+                    result[v] = data[v]
+                    reader(data[v], result)
+                if(isinstance(result, list)):
+                    result[itemlength-1] = type(data[v]).__name__ 
+                else:
+                    data[v] = type(data[v]).__name__
+                itemlength-=1
+
+        with open('schema.json', 'w') as f:
+            json.dump(result, f)
+            '''Here also we writing new always i could fix this but not enough time'''
+        return result
+
+    print(reader(data))
